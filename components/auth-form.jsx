5940c26bca9f4221d8f52909cace70acaa3f30f9
Ric Mershon <rmersh@ricmershon.com>
2024-11-01 12:25:34 -0400

    Input validation

--- components/auth-form.jsx
+++ components/auth-form.jsx
@@ -1,8 +1,15 @@
+'use client';
+
+import { useFormState } from 'react-dom';
 import Link from 'next/link';
 
+import { signup } from '@/lib/actions';
+
 export default function AuthForm() {
+    const [formState, formAction] = useFormState(signup, {});
+
     return (
-        <form id="auth-form">
+        <form id="auth-form" action={formAction}>
             <div>
                 <img src="/images/auth-icon.jpg" alt="A lock icon" />
             </div>
@@ -14,6 +21,15 @@ export default function AuthForm() {
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" />
             </p>
+            {formState.errors && (
+                <ul id="form-errors">
+                    {Object.keys(formState.errors).map((error) => (
+                        <li key={error}>
+                            {formState.errors[error]}
+                        </li>
+                    ))}
+                </ul>
+            )}
             <p>
                 <button type="submit">
                     Create Account
